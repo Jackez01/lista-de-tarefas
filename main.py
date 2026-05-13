e3cb11969cb1f812313b76bce8b8e4ce6e62a231
Jackez01 <lucasgefreitas@gmail.com>
2026-05-13 19:05:47 -0300

    Criação inicial das rotas

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models, schemas
+from database import engine, SessionLocal
+from typing import List
+from sqlalchemy.orm import joinedload
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
