f30a4fdf36f05978dcdfc201dbd2b6c4be0f2e44
Jackez01 <lucasgefreitas@gmail.com>
2026-05-19 18:41:55 -0300

    listar e deletar tarefas feito

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 import schemas, database, models
 from database import engine, SessionLocal
 from models import Tarefa
+from typing import List
 
 # variavavel do app
 app = FastAPI()
@@ -16,18 +17,28 @@ def get_db():
         db.close()
 
 #criando a rota
-@app.get('/tarefas/')
-def lista_tarefas():
-    tarefas = {'Tarefa': 'Lavar a casa'}
+@app.get('/tarefas/', response_model = List[schemas.TarefaResponse])
+def lista_tarefas(db: Session = Depends(get_db)):
+    tarefas = db.query(models.Tarefa).all()
     return tarefas
+    
 
 # criando a tabela no postgreSQL
 database.Base.metadata.create_all(bind=engine)
 
-@app.post('/tarefas/')
+@app.post('/tarefas/', response_model = schemas.TarefaResponse)
 def criar_tarefa(tarefa: schemas.TarefaCreate, db: Session = Depends(get_db)):
     db_tarefas = models.Tarefa(titulo = tarefa.titulo, descricao = tarefa.descricao, status = tarefa.status)
     db.add(db_tarefas)
     db.commit()
     db.refresh(db_tarefas)
     return db_tarefas
+
+# Criando rota para deletar
+@app.delete('/tarefas/{id}')
+def deletar_tarefa(id: int, db: Session = Depends(get_db)):
+    excluir = db.query(models.Tarefa).filter(models.Tarefa.id == id).first()
+
+    db.delete(excluir)
+    db.commit()
+    return excluir
--- schemas.py
+++ schemas.py
@@ -4,3 +4,12 @@ class TarefaCreate(BaseModel):
     titulo: str
     descricao: str
     status: str
+
+class TarefaResponse(BaseModel):
+    id: int
+    titulo: str
+    descricao: str
+    status: str
+
+class Config:
+    from_attributes = True
